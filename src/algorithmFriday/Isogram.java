874fb950d7a62a5da18609f57b5641a4927ddd40
Samson Aderibigbe <samsontimilehin@gmail.com>
2021-08-30 21:46:55 +0100

    update

--- src/algorithmFriday/Isogram.java
+++ src/algorithmFriday/Isogram.java
@@ -6,34 +6,33 @@ public class Isogram {
 
     public static void main(String[] args) {
 
-        isIsogram("   ");
+        String isString = "thence";
+        boolean result = isIsogram(isString);
+        System.out.println(result);
 
     }
 
-    public static void isIsogram(String stringValue) {
+    public static boolean isIsogram(String stringValue) {
 
 
-        StringBuilder storeCharacters = new StringBuilder();
+
 
         for (int i = 0; i < stringValue.length(); i++) {
 
             for (int j = i + 1; j < stringValue.length(); j++) {
 
                 if (stringValue.charAt(i) == stringValue.charAt(j)) {
-                    storeCharacters.append(stringValue.charAt(j));
+
+                    return true;
                 }
+
 
             }
         }
-        if(storeCharacters.length() > 0){
 
-            System.out.println("It is not an Isogram");
-        }
-        if(storeCharacters.isEmpty() ){
 
-            System.out.println("It is an Isogram");
 
-        }
+        return false;
     }
 }
 
