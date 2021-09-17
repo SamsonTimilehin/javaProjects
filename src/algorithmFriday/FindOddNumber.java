217d0cce21daa12d9db76a421ecd819a43c65c3e
Samson Aderibigbe <samsontimilehin@gmail.com>
2021-09-17 12:07:49 +0100

    New exercise

--- src/algorithmFriday/FindOddNumber.java
+++ src/algorithmFriday/FindOddNumber.java
@@ -4,16 +4,26 @@ public class FindOddNumber {
 
     public static void main(String[] args) {
 
-        displayOddNumber(2,20);
+        displayOddNumber(20,2);
     }
 
     public static void displayOddNumber(int firstNum, int secondNum){
 
+        if(firstNum < secondNum) {
 
-        for(int i = firstNum; i < secondNum; i++){
+            for (int i = firstNum; i < secondNum; i++) {
 
-            if( i % 2 != 0){
-                System.out.println(i);
+                if (i % 2 != 0) {
+                    System.out.println(i);
+                }
+            }
+        }else if(firstNum > secondNum){
+
+            for(int i = secondNum; i < firstNum; i++){
+
+                if(i % 2 != 0){
+                    System.out.println(i);
+                }
             }
         }
     }
